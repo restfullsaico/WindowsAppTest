73a9da17355b5ad2c56ad4cab8d9c822f600563b
restfullsaico <restfullsaico@gmail.com>
2016-11-05 14:31:22 -0500

    boton azul

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button1.Location = new System.Drawing.Point(26, 125);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(228, 23);
